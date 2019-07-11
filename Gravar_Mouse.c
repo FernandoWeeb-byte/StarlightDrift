ec24c2811ea5750d121d6b743249544a8822c9af
lucasflins <48838426+lucasflins@users.noreply.github.com>
2019-07-11 18:41:32 -0300

    Gravar mouse com WASD

--- Gravar_Mouse.c
+++ Gravar_Mouse.c
@@ -1,18 +1,42 @@
 #include "raylib.h"
 #include <stdio.h>
 
+Vector2 mouse;
+
+void Movimento() //Função de movimentação
+{
+    if(IsKeyDown('W'))
+    {
+        mouse.y -= 2;
+    }
+    if(IsKeyDown('A'))
+    {
+        mouse.x -= 2;
+    }
+    if(IsKeyDown('S'))
+    {
+        mouse.y += 2;
+    }
+    if(IsKeyDown('D'))
+    {
+        mouse.x += 2;
+    }
+    
+}
+
 int main(void){
-    Vector2 mouse;
+    
     FILE * inimigos = fopen ("/raylib/StarlightDrift/enemy/enemies.txt","w");
     int z=0;
     SetTargetFPS(60);
-    InitWindow(1366,768,"I Like Good Pussy and I Like Good Trees");
-    ToggleFullscreen();
+    InitWindow(720,876,"I Like Good Pussy and I Like Good Trees");
+    //ToggleFullscreen();
     while(!WindowShouldClose()){
+        Movimento();
         BeginDrawing();
         ClearBackground(ORANGE);
-        mouse=GetMousePosition();
-        DrawText(TextFormat("%i %i",(int)mouse.x,(int)mouse.y),mouse.x-35,mouse.y+20,20,BLUE);
+        DrawText(TextFormat("%i %i",(int)mouse.x,(int)mouse.y),200,200,20,BLUE);
+        DrawCircle(mouse.x,mouse.y,1,BLUE);
         if (IsKeyPressed(KEY_G)){
             z++;
         }
